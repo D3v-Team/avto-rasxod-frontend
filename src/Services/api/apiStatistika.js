bfc2e8694393ed05ec94f2c96e2e55b1d6bdfc3a
nrwy77 <qurbonboyevaziz@gmail.com>
2026-07-23 12:38:20 +0500

    edit

---
+++ src/Services/api/apiStatistika.js
@@ -0,0 +1,56 @@
+// Services/api/apiStatistika.js
+import { $api } from "../parametres/axios";
+
+
+class apiStatistika {
+  static AllEmployeesAndCarsCounts = async () => {
+    const response = await $api.get(
+      "/car-daily-expenses/all-employees-and-cars-counts",
+    );
+    return response.data;
+  };
+
+  static MonthlyStatistics = async (month, { is_active, car_id } = {}) => {
+    const params = { month };
+    if (is_active !== undefined) params.is_active = is_active;
+    if (car_id) params.car_id = car_id;
+
+    const response = await $api.get(
+      "/car-daily-expenses/monthly-statistics",
+      { params },
+    );
+    return response.data;
+  };
+
+  static MonthlySummary = async (carId, month) => {
+    const response = await $api.get(
+      `/car-daily-expenses/monthly-summary/${carId}`,
+      { params: { month } },
+    );
+    return response.data;
+  };
+
+  static CarMonthlyReport = async (carId, month, fuelId) => {
+    const params = { car_id: carId, month };
+    if (fuelId) params.fuel_id = fuelId;
+
+    const response = await $api.get(
+      "/car-daily-expenses/car-monthly-report",
+      { params },
+    );
+    return response.data;
+  };
+
+  static YearlyStatistics = async (year, { car_id } = {}) => {
+    const params = { year };
+    if (car_id) params.car_id = car_id;
+
+    const response = await $api.get(
+      "/car-daily-expenses/yearly-statistics",
+      { params },
+    );
+    return response.data;
+  };
+}
+
+export { apiStatistika };
