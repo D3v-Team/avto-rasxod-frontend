97b95b9dd71815a05f0853d0d57295beeeb4ded6
Doston <doston@Mac.mshome.net>
2026-08-06 10:31:50 +0500

    fixx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -142,7 +142,9 @@ export default function Login() {
       {/* O‘ng taraf – forma */}
       <Flex
         w={{ base: "100%", md: "50%", lg: "45%", xl: "40%" }}
-        h="100vhr"
+        h="100vh"
+        align
+        ="center"
         justify="center"
         p={{ base: 6, sm: 10, md: 12 }}
         bg="surface"
